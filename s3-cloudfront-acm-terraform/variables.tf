642c750b6d2245ca543d927114b179cd59236fb0
ikramhm <98710900+ikramhm@users.noreply.github.com>
2022-11-22 21:46:02 +0000

    Delete s3-cloudfront-acm-terraform directory

--- s3-cloudfront-acm-terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ssl_certificate_arn" {
-    default = "s"
-    #${{ secrets.MSB-SSL-CERT-ARN }}
-}
-
-variable "invalidate_cloud_front_on_s3_update" {
-  default     = true
-  description = "Setup lambda to invalidate CDN on S3 updates"
-}
